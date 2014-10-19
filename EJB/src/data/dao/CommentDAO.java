d30c5392c22b2a8dd8dad2d5afbbccc858a05c21
Skarx <skarx69@gmail.com>
2014-10-19 19:47:09 +0200

    DAO

---
+++ EJB/src/data/dao/CommentDAO.java
@@ -0,0 +1,80 @@
+package data.dao;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import data.model.Comment;
+
+import java.util.List;
+/**
+ * Created by Manfred on 27/10/2014.
+ */
+@Stateless
+@LocalBean
+public class CommentDAO {
+    //-----------------------------------------------------------------------------
+    /**
+     * Référence vers le gestionnaire de persistance.
+     */
+    @PersistenceContext
+    EntityManager entityManager;
+    //-----------------------------------------------------------------------------
+    /**
+     * Default constructor.
+     */
+    private VacationDAO vacationDAO;
+    public CommentDAO()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    //-----------------------------------------------------------------------------
+    public Comment findById(Integer id)
+    {
+        return entityManager.find(Comment.class, id);
+    }
+    //----------------------------------------------------------------------------
+    public List<Comment> findAll()
+    {
+        Query query = entityManager.createQuery(
+                "SELECT Comment FROM Comment Comment "
+                        + "ORDER BY Comment.id DESC");
+        List l = query.getResultList();
+
+        return (List<Comment>)l;
+    }
+    //-----------------------------------------------------------------------------
+
+    public List<Comment> findByVacation(int vacationId){
+        return (List<Comment>)vacationDAO.findById(vacationId).getComments();
+    }
+    //-----------------------------------------------------------------------------
+
+
+    public List<Comment> findByCreator(int creatorId){
+        Query query = entityManager.createQuery("" +
+                "SELECT Comment FROM Comment Comment " +
+                "WHERE Comment.managerId IS " + creatorId);
+        return query.getResultList();
+    }
+    //-----------------------------------------------------------------------------
+
+    public Comment persist(Comment Comment){
+        entityManager.persist(Comment);
+        return Comment;
+
+    }
+    //-----------------------------------------------------------------------------
+
+    public Comment update (Comment Comment){
+        return entityManager.merge(Comment);
+
+    }
+    //-----------------------------------------------------------------------------
+
+    public void remove(Comment Comment){
+        entityManager.remove(entityManager.merge(Comment));
+    }
+
+}
---
+++ EJB/src/data/dao/VacationDAO.java
@@ -0,0 +1,110 @@
+package data.dao;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import data.model.Vacation;
+import data.model.Status;
+import java.util.List;
+
+/**
+ * Created by Manfred on 27/10/2014.
+ */
+
+@Stateless
+@LocalBean
+public class VacationDAO {
+    //-----------------------------------------------------------------------------
+    /**
+     * Référence vers le gestionnaire de persistance.
+     */
+    @PersistenceContext
+    EntityManager entityManager;
+    //-----------------------------------------------------------------------------
+    /**
+     * Default constructor.
+     */
+    public VacationDAO()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    //-----------------------------------------------------------------------------
+    public Vacation findById(Integer id)
+    {
+        return entityManager.find(Vacation.class, id);
+    }
+    //----------------------------------------------------------------------------
+    public List<Vacation> findAll()
+    {
+        Query query = entityManager.createQuery(
+                "SELECT Vacation FROM Vacation Vacation "
+                        + "ORDER BY Vacation.id DESC");
+        List l = query.getResultList();
+
+        return (List<Vacation>)l;
+    }
+    //-----------------------------------------------------------------------------
+    public List<Vacation> findByManager(int managerId){
+        Query query = entityManager.createQuery("" +
+                "SELECT Vacation FROM Vacation Vacation " +
+                "WHERE Vacation.managerId IS " + managerId);
+        return query.getResultList();
+    }
+    //-----------------------------------------------------------------------------
+    public List<Vacation> findByHr(int hrId){
+        Query query = entityManager.createQuery("" +
+                "SELECT Vacation FROM Vacation Vacation " +
+                "WHERE Vacation.hrId IS " + hrId);
+        return query.getResultList();
+    }
+    //-----------------------------------------------------------------------------
+    public List<Vacation> findByEmployee(int employeeId){
+        Query query = entityManager.createQuery("" +
+                "SELECT Vacation FROM Vacation Vacation " +
+                "WHERE Vacation.employeeId IS " + employeeId);
+        return query.getResultList();
+    }
+    //-----------------------------------------------------------------------------
+    public List<Vacation> findByEmployeeAndStatus(int employeeId, Status status){
+        Query query = entityManager.createQuery("" +
+                "SELECT Vacation FROM Vacation Vacation " +
+                "WHERE Vacation.employeeId IS " + employeeId + " " +
+                "AND Vacations.status IS " + status.toString());
+        return query.getResultList();
+    }
+    //-----------------------------------------------------------------------------
+    public List<Vacation> findByManagerAndStatus(int managerId, Status status){
+        Query query = entityManager.createQuery("" +
+                "SELECT Vacation FROM Vacation Vacation " +
+                "WHERE Vacation.managerId IS " + managerId + " " +
+                "AND Vacations.status IS " + status.toString());
+        return query.getResultList();
+    }
+    //-----------------------------------------------------------------------------
+    public List<Vacation> findByHrAndStatus(int hrId, Status status){
+        Query query = entityManager.createQuery("" +
+                "SELECT Vacation FROM Vacation Vacation " +
+                "WHERE Vacation.hrId IS " + hrId + " " +
+                "AND Vacations.status IS " + status.toString());
+        return query.getResultList();
+    }
+    //-----------------------------------------------------------------------------
+    public Vacation persist(Vacation Vacation){
+        entityManager.persist(Vacation);
+        return Vacation;
+
+    }
+    //-----------------------------------------------------------------------------
+
+    public Vacation update (Vacation Vacation){
+        return entityManager.merge(Vacation);
+
+    }
+    //-----------------------------------------------------------------------------
+
+    public void remove(Vacation Vacation){
+        entityManager.remove(entityManager.merge(Vacation));
+    }
+
+}
