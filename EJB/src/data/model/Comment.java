042c112eeab193fe8915f2ae25e2c5ba57f5b0d1
Skarx <skarx69@gmail.com>
2014-10-19 19:32:41 +0200

    Initial commit

---
+++ EJB/src/data/model/Comment.java
@@ -0,0 +1,84 @@
+package data.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * Created by Hervé on 17/10/2014.
+ */
+@Entity
+public class Comment {
+    private int id;
+    private Timestamp creadate;
+    private String comments;
+    private Employee creator;
+    private Vacation vacation;
+
+    @Id
+    @Column(name = "id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "creatorId")
+    public Employee getCreator(){
+        return creator;
+    }
+    public void setCreator(Employee emp){
+        creator = emp;
+    }
+    @ManyToOne
+    @JoinColumn(name = "vacationId")
+    public Vacation getVacation(){
+        return vacation;
+    }
+    public void setVacation(Vacation vac){
+        vacation = vac;
+    }
+    @Basic
+    @Column(name = "creadate", nullable = false, insertable = true, updatable = true)
+    public Timestamp getCreadate() {
+        return creadate;
+    }
+
+    public void setCreadate(Timestamp creadate) {
+        this.creadate = creadate;
+    }
+
+    @Basic
+    @Column(name = "comments", nullable = false, insertable = true, updatable = true, length = 500)
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Comment comment1 = (Comment) o;
+
+        if (id != comment1.id) return false;
+        if (comments != null ? !comments.equals(comment1.comments) : comment1.comments != null) return false;
+        if (creadate != null ? !creadate.equals(comment1.creadate) : comment1.creadate != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (creadate != null ? creadate.hashCode() : 0);
+        result = 31 * result + (comments != null ? comments.hashCode() : 0);
+        return result;
+    }
+}
---
+++ EJB/src/data/model/Vacation.java
@@ -0,0 +1,140 @@
+package data.model;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.Set;
+
+/**
+ * Created by Hervé on 17/10/2014.
+ */
+@Entity
+public class Vacation {
+    private int id;
+    private Date begdate;
+    private String begtime;
+    private Date enddate;
+    private String endtime;
+    private String status;
+    private Employee employee;
+    private Employee manager;
+    private Employee hr;
+    private Set<Comment> comments;
+
+    @Id
+    @Column(name = "id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "employeeId")
+    public Employee getEmployee(){return employee;}
+
+    public void setEmployee(Employee employee){this.employee = employee;}
+
+    @ManyToOne
+    @JoinColumn(name = "managerId")
+
+    public Employee getManager(){return manager;}
+
+    public void setManager(Employee manager){this.manager = manager;}
+
+    @ManyToOne
+    @JoinColumn(name = "hrId")
+    public Employee getHr(){return hr;}
+
+    public void setHr(Employee hr){this.hr = hr;}
+
+    @OneToMany(mappedBy = "vacation")
+    public Set<Comment> getComments(){return comments;}
+
+    public void setComments(Set<Comment> comments) {this.comments = comments;}
+
+    public void addComments(Comment comment){
+        comments.add(comment);
+    }
+    public void removeComments(Comment comment){
+        comments.remove(comment);
+    }
+    @Basic
+    @Column(name = "begdate", nullable = false, insertable = true, updatable = true)
+    public Date getBegdate() {
+        return begdate;
+    }
+
+    public void setBegdate(Date begdate) {
+        this.begdate = begdate;
+    }
+
+    @Basic
+    @Column(name = "begtime", nullable = false, insertable = true, updatable = true, length = 20)
+    public String getBegtime() {
+        return begtime;
+    }
+
+    public void setBegtime(String begtime) {
+        this.begtime = begtime;
+    }
+
+    @Basic
+    @Column(name = "enddate", nullable = false, insertable = true, updatable = true)
+    public Date getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(Date enddate) {
+        this.enddate = enddate;
+    }
+
+    @Basic
+    @Column(name = "endtime", nullable = false, insertable = true, updatable = true, length = 20)
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = false, insertable = true, updatable = true, length = 20)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vacation vacation = (Vacation) o;
+
+        if (id != vacation.id) return false;
+        if (begdate != null ? !begdate.equals(vacation.begdate) : vacation.begdate != null) return false;
+        if (begtime != null ? !begtime.equals(vacation.begtime) : vacation.begtime != null) return false;
+        if (enddate != null ? !enddate.equals(vacation.enddate) : vacation.enddate != null) return false;
+        if (endtime != null ? !endtime.equals(vacation.endtime) : vacation.endtime != null) return false;
+        if (status != null ? !status.equals(vacation.status) : vacation.status != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (begdate != null ? begdate.hashCode() : 0);
+        result = 31 * result + (begtime != null ? begtime.hashCode() : 0);
+        result = 31 * result + (enddate != null ? enddate.hashCode() : 0);
+        result = 31 * result + (endtime != null ? endtime.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
+}
